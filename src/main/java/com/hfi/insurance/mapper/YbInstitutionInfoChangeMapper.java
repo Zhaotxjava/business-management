c763ccf3d4767ddd0634fa3a914c38a56e48a6f6
your Name <tt.@com>
2021-09-08 17:36:55 +0800

    记录机构信息变更表

---
+++ src/main/java/com/hfi/insurance/mapper/YbInstitutionInfoChangeMapper.java
@@ -0,0 +1,10 @@
+package com.hfi.insurance.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import com.hfi.insurance.model.YbInstitutionInfoChange;
+
+public interface YbInstitutionInfoChangeMapper  extends BaseMapper<YbInstitutionInfoChange> {
+
+
+}
