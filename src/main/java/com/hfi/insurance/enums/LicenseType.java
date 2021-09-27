31233bcd4253eb499333ec31d8078f04e958aca6
loong <75986562@qq.com>
2021-09-27 15:55:50 +0800

    更新创建机构和更新机构信息的证件类型判断

---
+++ src/main/java/com/hfi/insurance/enums/LicenseType.java
@@ -0,0 +1,19 @@
+package com.hfi.insurance.enums;
+
+/**
+ * @author jthealth-NZH
+ * @Date 2021/9/27 14:12
+ * @Describe
+ * @Version 1.0
+ */
+public enum LicenseType {
+    /**
+     * 社会信用代码
+     */
+    SOCNO,
+    /**
+     * 许可证号
+     */
+    OTHERNO;
+
+}
