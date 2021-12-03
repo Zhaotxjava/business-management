406f10bab79cfd034025c92b2e59373280557008
loong <75986562@qq.com>
2021-12-03 10:55:02 +0800

    [add]1.增加签署日期精确定位功能

--- src/main/java/com/hfi/insurance/model/sign/req/SignTimeBean.java
+++ src/main/java/com/hfi/insurance/model/sign/req/SignTimeBean.java
@@ -11,11 +11,12 @@ import lombok.Data;
 @Data
 @ApiModel("签章日期信息")
 public class SignTimeBean {
+    private String id;
     private String dateFormat;
     private Integer fontSize;
     private String posPage;
-    private Float posX;
-    private Float posY;
+    private Integer posX;
+    private Integer posY;
 
     public SignTimeBean() {
     }
