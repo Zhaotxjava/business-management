e6ebfc92257fec92f489dcc14fe3a4c719dd1e0e
huyk <a123456789>
2022-04-19 14:56:03 +0800

    流程状态改变异步回调接口逻辑修改: 1.当ACTION为SIGN_FLOW_FINISH且organizeNo为空时,根据SIGN_FLOW_ID更新FLOW_STATUS; 2.当ACTION为SIGN_FLOW_FINISH且organizeNo不为空时,根据SIGN_FLOW_ID和NUMBER更新FLOW_STATUS; 3.当ACTION为SIGN_FLOW_UPDATE且organizeNo不为空时,根据SIGN_FLOW_ID和NUMBER更新SIGN_STATUS; 4.其他情况均不更新数据;

--- src/main/java/com/hfi/insurance/service/impl/YbFlowInfoServiceImpl.java
+++ src/main/java/com/hfi/insurance/service/impl/YbFlowInfoServiceImpl.java
@@ -46,84 +46,90 @@ public class YbFlowInfoServiceImpl extends ServiceImpl<YbFlowInfoMapper, YbFlowI
 		return baseMapper.selectPage(page, queryWrapper);
 	}
 
-    @Override
-    public GetSignedRecordBatchRes getSignedRecord(String institutionNumber, GetRecordInfoBatchReq req) {
-        log.info("getSignedRecord input institutionNumber = {},req = {}", institutionNumber, JSONObject.toJSONString(req));
+	@Override
+	public GetSignedRecordBatchRes getSignedRecord(String institutionNumber, GetRecordInfoBatchReq req) {
+		log.info("getSignedRecord input institutionNumber = {},req = {}", institutionNumber, JSONObject.toJSONString(req));
 //        Page<YbFlowInfo> page = new Page<>(req.getPageNum(), req.getPageSize());
-        QueryWrapper<YbFlowInfo> queryWrapper = pkQueryWrapperByBatch(institutionNumber, req);
-        List<YbFlowInfo> list = baseMapper.selectList(queryWrapper);
-        Set<String> result = new HashSet<>();
-        Set<String> successSet = new HashSet<>();
-        Set<String> failSet = new HashSet<>();
-        Map<String,String> inputNameMap = new HashMap<>();
-        BatchQueryTypeEnum type = BatchQueryTypeEnum.getType(req.getQueryType());
-        if(NAMES == type){
-            List<YbInstitutionInfo> infoList = iYbInstitutionInfoService.getInstitutionInfoByName(req.getNumbers());
-            infoList.forEach(info ->{
-                inputNameMap.put(info.getNumber(),info.getInstitutionName());
-            });
-        }
+		QueryWrapper<YbFlowInfo> queryWrapper = pkQueryWrapperByBatch(institutionNumber, req);
+		List<YbFlowInfo> list = baseMapper.selectList(queryWrapper);
+		Set<String> result = new HashSet<>();
+		Set<String> successSet = new HashSet<>();
+		Set<String> failSet = new HashSet<>();
+		Map<String, String> inputNameMap = new HashMap<>();
+		BatchQueryTypeEnum type = BatchQueryTypeEnum.getType(req.getQueryType());
+		if (NAMES == type) {
+			List<YbInstitutionInfo> infoList = iYbInstitutionInfoService.getInstitutionInfoByName(req.getNumbers());
+			infoList.forEach(info -> {
+				inputNameMap.put(info.getNumber(), info.getInstitutionName());
+			});
+		}
 
-        //遍历结果集
-        list.forEach(y -> {
-            //将结果添加到结果集合
-            result.add(y.getSignFlowId());
-            switch (type) {
-                case NUMBERS:
-                    successSet.add(y.getNumber());
-                    break;
-                case NAMES://getInstitutionInfoByName
-                    successSet.add(inputNameMap.get(y.getNumber()));
-                    break;
-                default:
-                    break;
-            }
-        });
+		//遍历结果集
+		list.forEach(y -> {
+			//将结果添加到结果集合
+			result.add(y.getSignFlowId());
+			switch (type) {
+				case NUMBERS:
+					successSet.add(y.getNumber());
+					break;
+				case NAMES://getInstitutionInfoByName
+					successSet.add(inputNameMap.get(y.getNumber()));
+					break;
+				default:
+					break;
+			}
+		});
 
-        if( BatchQueryTypeEnum.NUMBERS == type || BatchQueryTypeEnum.NAMES == type){
-            req.getNumbers().forEach(inputParam -> {
-                if(!successSet.contains(inputParam)){
-                    failSet.add(inputParam);
-                }
-            });
-        }
+		if (BatchQueryTypeEnum.NUMBERS == type || BatchQueryTypeEnum.NAMES == type) {
+			req.getNumbers().forEach(inputParam -> {
+				if (!successSet.contains(inputParam)) {
+					failSet.add(inputParam);
+				}
+			});
+		}
 
-        GetSignedRecordBatchRes res = new GetSignedRecordBatchRes(result,failSet);
-        return res;
-    }
-
-    @Override
-    public List<YbFlowInfo> getSignedRecordByAreaCode(String institutionNumber) {
-        QueryWrapper<YbFlowInfo> queryWrapper = new QueryWrapper<>();
-        queryWrapper.select("distinct sign_flow_id").likeRight("number", institutionNumber);
-        queryWrapper.eq("flow_status", "2");
-        queryWrapper.and(i -> i.isNull("batch_status").or().eq("batch_status", Cons.BatchStr.BATCH_STATUS_SUCCESS));
-        queryWrapper.orderByDesc("initiator_time");
-        List<YbFlowInfo> list = baseMapper.selectList(queryWrapper);
-        log.info("getSignedRecordByAreaCode result list = {}",JSONObject.toJSONString(list));
-        return list;
-    }
-
-    @Override
-    public Page<YbFlowInfo> getSignedRecordByBatchDownload(String institutionNumber, GetRecordInfoReq req) {
-        return null;
-    }
-
-    @Override
-    public Integer getSignedRecordCount(String institutionNumber, GetRecordInfoReq req) {
-        QueryWrapper<YbFlowInfo> queryWrapper = pkQueryWrapper(institutionNumber, req);
-        return baseMapper.selectCount(queryWrapper);
-    }
+		GetSignedRecordBatchRes res = new GetSignedRecordBatchRes(result, failSet);
+		return res;
+	}
 
 	@Override
-	public int updateFlowStatusBySignFlowId(Integer status, String signFlowId) {
-		YbFlowInfo ybFlowInfo = new YbFlowInfo();
-		ybFlowInfo.setFlowStatus(status);
+	public List<YbFlowInfo> getSignedRecordByAreaCode(String institutionNumber) {
+		QueryWrapper<YbFlowInfo> queryWrapper = new QueryWrapper<>();
+		queryWrapper.select("distinct sign_flow_id").likeRight("number", institutionNumber);
+		queryWrapper.eq("flow_status", "2");
+		queryWrapper.and(i -> i.isNull("batch_status").or().eq("batch_status", Cons.BatchStr.BATCH_STATUS_SUCCESS));
+		queryWrapper.orderByDesc("initiator_time");
+		List<YbFlowInfo> list = baseMapper.selectList(queryWrapper);
+		log.info("getSignedRecordByAreaCode result list = {}", JSONObject.toJSONString(list));
+		return list;
+	}
 
+	@Override
+	public Page<YbFlowInfo> getSignedRecordByBatchDownload(String institutionNumber, GetRecordInfoReq req) {
+		return null;
+	}
+
+	@Override
+	public Integer getSignedRecordCount(String institutionNumber, GetRecordInfoReq req) {
+		QueryWrapper<YbFlowInfo> queryWrapper = pkQueryWrapper(institutionNumber, req);
+		return baseMapper.selectCount(queryWrapper);
+	}
+
+	@Override
+	public int updateBySignFlowId(YbFlowInfo updateInfo, String signFlowId) {
 		UpdateWrapper<YbFlowInfo> updateWrapper = new UpdateWrapper<>();
 		updateWrapper.eq("sign_flow_id", signFlowId);
 
-		return baseMapper.update(ybFlowInfo, updateWrapper);
+		return baseMapper.update(updateInfo, updateWrapper);
+	}
+
+	@Override
+	public int updateBySignFlowIdAndNumber(YbFlowInfo updateInfo, String signFlowId, String number) {
+		UpdateWrapper<YbFlowInfo> updateWrapper = new UpdateWrapper<>();
+		updateWrapper.eq("sign_flow_id", signFlowId);
+		updateWrapper.eq("number", number);
+
+		return baseMapper.update(updateInfo, updateWrapper);
 	}
 
 	public QueryWrapper<YbFlowInfo> pkQueryWrapper(String institutionNumber, GetRecordInfoReq req) {
@@ -153,28 +159,28 @@ public class YbFlowInfoServiceImpl extends ServiceImpl<YbFlowInfoMapper, YbFlowI
 		queryWrapper.orderByDesc("initiator_time");
 
 
-        //queryWrapper.ne("batch_status", Cons.BatchStr.BATCH_STATUS_FAIL);
-        return queryWrapper;
-    }
+		//queryWrapper.ne("batch_status", Cons.BatchStr.BATCH_STATUS_FAIL);
+		return queryWrapper;
+	}
 
-    public QueryWrapper<YbFlowInfo> pkQueryWrapperByBatch(String institutionNumber, GetRecordInfoBatchReq req) {
-        QueryWrapper<YbFlowInfo> queryWrapper = new QueryWrapper<>();
-        //通过机构编码查询符合条件的流程ID
-        List<YbFlowInfo> list = getSignedRecordByAreaCode(institutionNumber);
-        if (!list.isEmpty()) {
-            List<String> signFlowIdList = new ArrayList<>();
-            list.forEach(y -> {
-                signFlowIdList.add(y.getSignFlowId());
-            });
-            log.info("list = {}", signFlowIdList.toString());
-            if (!signFlowIdList.isEmpty()) {
-                queryWrapper.in("sign_flow_id", signFlowIdList);
-            }
-        }
-        //
-        if (StringUtils.isNotBlank(req.getSubject())) {
-            queryWrapper.likeRight("subject", req.getSubject());
-        }
+	public QueryWrapper<YbFlowInfo> pkQueryWrapperByBatch(String institutionNumber, GetRecordInfoBatchReq req) {
+		QueryWrapper<YbFlowInfo> queryWrapper = new QueryWrapper<>();
+		//通过机构编码查询符合条件的流程ID
+		List<YbFlowInfo> list = getSignedRecordByAreaCode(institutionNumber);
+		if (!list.isEmpty()) {
+			List<String> signFlowIdList = new ArrayList<>();
+			list.forEach(y -> {
+				signFlowIdList.add(y.getSignFlowId());
+			});
+			log.info("list = {}", signFlowIdList.toString());
+			if (!signFlowIdList.isEmpty()) {
+				queryWrapper.in("sign_flow_id", signFlowIdList);
+			}
+		}
+		//
+		if (StringUtils.isNotBlank(req.getSubject())) {
+			queryWrapper.likeRight("subject", req.getSubject());
+		}
 //        if (StringUtils.isNotBlank(req.getSignStatus())) {
 //            queryWrapper.eq("sign_status", req.getSignStatus());
 //        }
@@ -184,59 +190,59 @@ public class YbFlowInfoServiceImpl extends ServiceImpl<YbFlowInfoMapper, YbFlowI
 //        if (null != req.getFlowStatus()) {
 //            queryWrapper.eq("flow_status", req.getFlowStatus());
 //        }
-        if (StringUtils.isNotEmpty(req.getBeginInitiateTime())) {
-            queryWrapper.ge("initiator_time", req.getBeginInitiateTime());
-        }
-        if (StringUtils.isNotEmpty(req.getEndInitiateTime())) {
-            //<=
-            queryWrapper.le("initiator_time", req.getEndInitiateTime());
-        }
-        BatchQueryTypeEnum type = BatchQueryTypeEnum.getType(req.getQueryType());
-        if (type == BatchQueryTypeEnum.BLANK || !req.getNumbers().isEmpty()) {
-            switch (type) {
-                case SIGNLE_NUMBER:
-                    queryWrapper.like("number", req.getNumbers().get(0));
+		if (StringUtils.isNotEmpty(req.getBeginInitiateTime())) {
+			queryWrapper.ge("initiator_time", req.getBeginInitiateTime());
+		}
+		if (StringUtils.isNotEmpty(req.getEndInitiateTime())) {
+			//<=
+			queryWrapper.le("initiator_time", req.getEndInitiateTime());
+		}
+		BatchQueryTypeEnum type = BatchQueryTypeEnum.getType(req.getQueryType());
+		if (type == BatchQueryTypeEnum.BLANK || !req.getNumbers().isEmpty()) {
+			switch (type) {
+				case SIGNLE_NUMBER:
+					queryWrapper.like("number", req.getNumbers().get(0));
 
-                    break;
-                case SIGNLE_NAME:
-                    //把机构名变为
-                    String name = req.getNumbers().get(0);
-                    //通过模糊查询，查询所有符合条件的机构number
-                    List<YbInstitutionInfo> list2 = iYbInstitutionInfoService.getInstitutionInfoByName(name);
-                    Set<String> tempNumber = new HashSet<>();
-                    list2.forEach(y -> {
-                        tempNumber.add(y.getNumber());
-                    });
-                    if (!tempNumber.isEmpty()) {
-                        queryWrapper.in("number", tempNumber);
-                    }
-                    break;
-                case NUMBERS:
-                    queryWrapper.in("number", req.getNumbers());
-                    break;
-                case NAMES://getInstitutionInfoByName
-                    //把机构名变为
-                    //通过查询，查询所有符合条件的机构number
-                    List<YbInstitutionInfo> list3 = iYbInstitutionInfoService.getInstitutionInfoByName(req.getNumbers());
-                    Set<String> tempNumber2 = new HashSet<>();
-                    list3.forEach(y -> {
-                        tempNumber2.add(y.getNumber());
-                    });
-                    if (!tempNumber2.isEmpty()) {
-                        queryWrapper.in("number", tempNumber2);
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
+					break;
+				case SIGNLE_NAME:
+					//把机构名变为
+					String name = req.getNumbers().get(0);
+					//通过模糊查询，查询所有符合条件的机构number
+					List<YbInstitutionInfo> list2 = iYbInstitutionInfoService.getInstitutionInfoByName(name);
+					Set<String> tempNumber = new HashSet<>();
+					list2.forEach(y -> {
+						tempNumber.add(y.getNumber());
+					});
+					if (!tempNumber.isEmpty()) {
+						queryWrapper.in("number", tempNumber);
+					}
+					break;
+				case NUMBERS:
+					queryWrapper.in("number", req.getNumbers());
+					break;
+				case NAMES://getInstitutionInfoByName
+					//把机构名变为
+					//通过查询，查询所有符合条件的机构number
+					List<YbInstitutionInfo> list3 = iYbInstitutionInfoService.getInstitutionInfoByName(req.getNumbers());
+					Set<String> tempNumber2 = new HashSet<>();
+					list3.forEach(y -> {
+						tempNumber2.add(y.getNumber());
+					});
+					if (!tempNumber2.isEmpty()) {
+						queryWrapper.in("number", tempNumber2);
+					}
+					break;
+				default:
+					break;
+			}
+		}
 
-        //只查成功的
-        queryWrapper.eq("flow_status", "2");
-        queryWrapper.and(i -> i.isNull("batch_status").or().eq("batch_status", Cons.BatchStr.BATCH_STATUS_SUCCESS));
-        queryWrapper.select("sign_flow_id");
-        //        queryWrapper.orderByDesc("initiator_time");
+		//只查成功的
+		queryWrapper.eq("flow_status", "2");
+		queryWrapper.and(i -> i.isNull("batch_status").or().eq("batch_status", Cons.BatchStr.BATCH_STATUS_SUCCESS));
+		queryWrapper.select("sign_flow_id");
+		//        queryWrapper.orderByDesc("initiator_time");
 //        queryWrapper.ne("batch_status", Cons.BatchStr.BATCH_STATUS_FAIL);
-        return queryWrapper;
-    }
+		return queryWrapper;
+	}
 }
